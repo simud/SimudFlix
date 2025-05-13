46ff771a4acd1781108a970cd6d67bd83eac4f46
Simud <149002960+simud@users.noreply.github.com>
2025-05-13 20:26:14 +0200

    Update build.gradle.kts

--- scraper/build.gradle.kts
+++ scraper/build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     implementation("org.jsoup:jsoup:1.18.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-    implementation("com.github.Blatzar:NiceHttp:0.4.11")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
